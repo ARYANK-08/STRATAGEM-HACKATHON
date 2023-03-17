cd982abe2682865f2a518e983ebc795d876a1d5d
Aryan Kyatham <120780784+ARYANK-08@users.noreply.github.com>
2023-03-18 00:55:40 +0530

    Update models.py

--- Start Fresh/website/models.py
+++ Start Fresh/website/models.py
@@ -15,3 +15,17 @@ class User(db.Model, UserMixin):
     password=db.Column(db.String(150))
     first_name=db.Column(db.String(150))
     notes= db.relationship('Note')
+
+class Info(db.Model):
+    p_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable = False)
+    email = db.Column(db.Text, nullable=False)
+    age = db.Column(db.Numeric, nullable=False)
+    mobileno = db.Column(db.Numeric, nullable=False) 
+    blood = db.Column(db.Text, nullable=False)
+    img = db.Column(db.Text, unique=True, nullable=False)
+    filename = db.Column(db.Text, nullable=False)
+    mimetype = db.Column(db.Text, nullable=False)
+    date = db.Column(db.String(255), nullable=True)
+
+
